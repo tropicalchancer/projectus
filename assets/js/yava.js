7d74541d48fa95f8610a2e94ca45bc75e6b84324
tropicalchancer <mcallisterchance@gmail.com>
2016-08-07 16:29:38 -0400

    imchi curation

---
+++ assets/js/yava.js
@@ -0,0 +1,12 @@
+$().ready(function(){
+
+
+
+$(function() {
+ $('.menulink').click(function(e){
+     e.preventDefault();
+   $("#bg").attr('src',"http://i2.wp.com/batman-news.com/wp-content/uploads/2014/01/jason-momoa-aquaman.jpg?quality=85&strip=info&w=800");
+ });
+});
+
+})
